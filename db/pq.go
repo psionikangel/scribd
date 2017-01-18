424b4f38058f4a2ff550d6ab6fb60da26b6e3cd8
Jérémie Carpentier-Roy <jcarpentier-roy@coginov.com>
2017-01-18 10:13:29 -0500

    fixed sql query expression ordering

--- db/pq.go
+++ db/pq.go
@@ -30,7 +30,7 @@ func NewRun(r models.Run) {
 func GetAllRuns() *models.Runlist {
 	client := getPQClient()
 	defer client.Close()
-	rows, err := client.Query(`select runid, starttime, endtime, machinename, filescount from run order by starttime desc group by machinename;`)
+	rows, err := client.Query(`select runid, starttime, endtime, machinename, filescount from run group by machinename order by starttime desc;`)
 	if err != nil {
 		panic(err)
 	}
